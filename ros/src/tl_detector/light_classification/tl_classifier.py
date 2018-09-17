12756320f0d99bf70674aeab579679ef6feb26ae
ZeyuT <41134031+ZeyuT@users.noreply.github.com>
2018-09-17 00:26:29 -0400

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -20,18 +20,18 @@ class TLClassifier(object):
         # load a frozen Tensorflow model into memory
         detection_graph = tf.Graph()
         with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
         
-        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-              
-        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-        self.detection_scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
-        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-        self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            self.detection_scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
         
         
     def load_image_into_numpy_array(image):
