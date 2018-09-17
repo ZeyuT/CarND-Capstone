5223fb120417fda34fac894b700ae7c02a5d441b
ZeyuT <41134031+ZeyuT@users.noreply.github.com>
2018-09-16 23:42:49 -0700

    Add files via upload

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -10,24 +10,30 @@ from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from glob import glob
+import sys
+from object_detection.utils import dataset_util
 
 class TLClassifier(object):
     def __init__(self):
-        
         MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-        PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
-        
+        PATH_TO_FROZEN_GRAPH = 'light_classification/'+MODEL_NAME+ '/frozen_inference_graph.pb'
+
+	# configuration for possible GPU use
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+
         # load a frozen Tensorflow model into memory
-        detection_graph = tf.Graph()
-        with detection_graph.as_default():
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
             with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-        
+            
+            self.sess = tf.Session(graph = self.detection_graph, config =  config)
             self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-
+	
             self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
             self.detection_scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
             self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
@@ -56,23 +62,22 @@ class TLClassifier(object):
         """
         # Run inference
         with self.detection_graph.as_default():
-             output_dict = self.sess.run(
+             (detection_boxes, detection_scores, detection_classes, num_detections) = self.sess.run(
                  [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
                  feed_dict={self.image_tensor: np.expand_dims(image, axis=0)})
                 
         # all outputs are float32 numpy arrays, so convert types as appropriate
-        output_dict['num_detections'] = int(output_dict['num_detections'][0])
-        output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
-        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-        output_dict['detection_scores'] = output_dict['detection_scores'][0]
+        detection_boxes=np.squeeze(detection_boxes)
+        detection_classes =np.squeeze(detection_classes)
+        detection_scores = np.squeeze(detection_scores)
         
-        id_vec = [i for i, v in enumerate(cls) if ((v == 10.) and (detection_scores[i] > 0.3))]
+        idx_vec = [i for i, v in enumerate(detection_classes.tolist()) if ((v == 10.) and (detection_scores[i] > 0.3))]
         
-        true_box = None;
+        true_box = [];
         if not (len(idx_vec) ==0):
             for idx in idx_vec:
                 dim = image.shape[0:2]
-                box = self.box_normal_to_pixel(boxes[idx], dim)
+                box = self.box_normal_to_pixel(detection_boxes[idx], dim)
                 box_h = box[2] - box[0]
                 box_w = box[3] - box[1]
                 
@@ -86,18 +91,23 @@ class TLClassifier(object):
         # Identify light color
         red_count = 0;
 
-        if true_box:
+        if len(true_box):
             # crop the input image       
-            resize_img = cv2.resize(processed_img[true_box[0]:true_box[2], true_box[1]:true_box[3]], (32, 32))
+            resize_img = cv2.resize(image[true_box[0]:true_box[2], true_box[1]:true_box[3]], (32, 32))
             # convert to hsv colorspace, to reduce the negative effect of bightness 
-            HSV_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            np_img = load_image_into_numpy_array(HSV_img)
-            for pixel in np_img:
+            HSV_img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV)
+	    
+	    pixel = 0
+            for row in range(32):
+		for col in range(32):
+		    pixel = HSV_img[row,col]
                 # in hsv space, hue value of red is roughly in 160-180 and 0-8
-                if((pixel[0] > 160 and pixel[0] < 180) or (pixel[0] > 0 and pixel[0] < 8)):
-                    red_count += 1
+                    if((pixel[0] > 160 and pixel[0] < 180) or (pixel[0] > 0 and pixel[0] < 8)):
+                        red_count += 1
             
-            if red_count > 200:
+	    print('red count:',red_count)
+            if red_count > 100:
                 return TrafficLight.RED
+		print ('detect red light')
             
         return TrafficLight.UNKNOWN
