a79e1cbc74bc5538944df8f83b9f2db6a5a0e14c
ZeyuT <41134031+ZeyuT@users.noreply.github.com>
2018-09-16 22:11:07 -0400

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,5 +1,16 @@
 from styx_msgs.msg import TrafficLight
 
+import numpy as np
+import cv2
+import tensorflow as tf
+from PIL import Image
+import os
+import six.moves.urllib as urllib
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from glob import glob
+
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
